f9db89156223ec45a3e01b5f4b83098bb14973cb
asiftuesdayboy <kayo0317soichiro@gmail.com>
2026-03-04 00:05:42 +0900

    Add particles configuration for particle.js

---
+++ particles-config.js
@@ -0,0 +1,42 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#60a5fa"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.3
+    },
+    size: {
+      value: 3
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#60a5fa",
+      opacity: 0.2,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 1.2
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: false
+      }
+    }
+  },
+  retina_detect: true
+});
